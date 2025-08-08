09ecc944d0be3b30774d1091b1b2b0f1faddcd75
sonephet53 <phetsone935@gmail.com>
2025-08-08 14:00:34 +0700

    b

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Nu/",
   plugins: [
     react(),
     tailwindcss(),
   ],
-  base: "/Nu/"
 })
